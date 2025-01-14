559c23b54d153ef1d6b548c390129e59467a7394
Xavier Ang <xaveang@gmail.com>
2025-01-14 19:19:42 +0800

    Change emmet plugin

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -44,6 +44,31 @@ return {
 				require("lspconfig").dockerls.setup({}),
 				require("lspconfig").cssls.setup({}),
 				require("lspconfig").html.setup({}),
+                require("lspconfig").emmet_language_server.setup({
+                    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+                    -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+                    -- **Note:** only the options listed in the table are supported.
+                    init_options = {
+                        ---@type table<string, string>
+                        includeLanguages = {},
+                        --- @type string[]
+                        excludeLanguages = {},
+                        --- @type string[]
+                        extensionsPath = {},
+                        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+                        preferences = {},
+                        --- @type boolean Defaults to `true`
+                        showAbbreviationSuggestions = true,
+                        --- @type "always" | "never" Defaults to `"always"`
+                        showExpandedAbbreviation = "always",
+                        --- @type boolean Defaults to `false`
+                        showSuggestionsAsSnippets = false,
+                        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+                        syntaxProfiles = {},
+                        --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+                        variables = {},
+                    },
+                })
 			})
 
             local cmp = require("cmp")
@@ -84,33 +109,33 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-            lspconfig.emmet_ls.setup({
-                -- on_attach = on_attach,
-                capabilities = capabilities,
-                filetypes = {
-                    "css",
-                    "eruby",
-                    "html",
-                    "javascript",
-                    "javascriptreact",
-                    "less",
-                    "sass",
-                    "typescript",
-                    "scss",
-                    "svelte",
-                    "pug",
-                    "typescriptreact",
-                    "vue",
-                },
-                init_options = {
-                    html = {
-                        options = {
-                            -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-                            ["bem.enabled"] = true,
-                        },
-                    },
-                },
-            })
+            -- lspconfig.emmet_ls.setup({
+            --     -- on_attach = on_attach,
+            --     capabilities = capabilities,
+            --     filetypes = {
+            --         "css",
+            --         "eruby",
+            --         "html",
+            --         "javascript",
+            --         "javascriptreact",
+            --         "less",
+            --         "sass",
+            --         "typescript",
+            --         "scss",
+            --         "svelte",
+            --         "pug",
+            --         "typescriptreact",
+            --         "vue",
+            --     },
+            --     init_options = {
+            --         html = {
+            --             options = {
+            --                 -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+            --                 ["bem.enabled"] = true,
+            --             },
+            --         },
+            --     },
+            -- })
 		end
 	},
 }
