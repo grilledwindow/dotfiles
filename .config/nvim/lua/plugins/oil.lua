f5038ef7976bc1f5b4599984f68731341d95bd32
Xavier Ang <xaveang@gmail.com>
2025-03-24 23:09:40 +0800

    Replace Netrw with Oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -128,14 +128,12 @@ return {
         -- Configuration for the floating window in oil.open_float
         float = {
             -- Padding around the floating window
-            padding = 2,
             -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
             max_width = 0,
             max_height = 0,
-            border = "rounded",
-            win_options = {
-                winblend = 0,
-            },
+            -- win_options = {
+            --     winblend = 0,
+            -- },
             -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
             get_win_title = nil,
             -- preview_split: Split direction: "auto", "left", "right", "above", "below".
@@ -177,10 +175,9 @@ return {
             min_height = { 5, 0.1 },
             -- optionally define an integer/float for the exact height of the preview window
             height = nil,
-            border = "rounded",
-            win_options = {
-                winblend = 0,
-            },
+            -- win_options = {
+            --     winblend = 0,
+            -- },
         },
         -- Configuration for the floating progress window
         progress = {
