84d42175b1a786b5b2efe90b39354c7775671bcb
Xavier Ang <xaveang@gmail.com>
2025-03-24 23:07:33 +0800

    nextls and typescript-tools

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -15,6 +15,24 @@ return {
 				-- see :help lsp-zero-keybindings
 				-- to learn the available actions
 				lsp.default_keymaps({buffer = bufnr})
+                local opts = { buffer = bufnr, noremap = true, silent = true }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+                vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
                 -- vim.api.nvim_create_autocmd("CursorHold", {
                 --     buffer = bufnr,
                 --     callback = function()
@@ -66,6 +84,17 @@ return {
                         --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
                         variables = {},
                     },
+                }),
+                require("lspconfig")["nextls"].setup({
+                    cmd = {"nextls", "--stdio"},
+                    init_options = {
+                        extensions = {
+                            credo = { enable = true }
+                        },
+                        experimental = {
+                            completions = { enable = true }
+                        }
+                    }
                 }),
             })
             local cmp = require("cmp")
